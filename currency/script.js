e4aba9ad76d5c30ae1f1a3f380ca93f2dc209d47
Raj kamal Singh <127596433+rajkamalsingh5@users.noreply.github.com>
2024-05-09 18:19:36 +0530

    Add files via upload

---
+++ currency/script.js
@@ -0,0 +1,212 @@
+const url = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+  };
+  const fromC=document.querySelector(".from select");
+  const toC=document.querySelector(".to select");
+  const info=document.querySelector(".info");
+
+  const dropDowns=document.querySelectorAll(".dropDown select");
+  const btn=document.querySelector("form button");
+for(let select of dropDowns)
+{
+    for(c in countryList)
+    {
+        let newOp=document.createElement("option");
+        newOp.innerText=c;
+        newOp.value=c;
+        if(select.name==="from"&&c==="INR")
+        {
+            newOp.selected="selected";
+        }else if(select.name==="to"&&c==="USD")
+        {
+            newOp.selected="selected";
+        }
+        select.append(newOp);
+    }
+    select.addEventListener("change",(e)=>{
+        flag(e.target);
+    })
+}
+const flag=(el)=>{
+    let codecurr=el.value;
+    let cuntry=countryList[codecurr];
+    let newP=`https://flagsapi.com/${cuntry}/flat/64.png`;
+    let img=el.parentElement.querySelector("img");
+    img.src=newP;
+}
+btn.addEventListener("click",async(e)=>{
+    e.preventDefault();
+    let paisa=document.querySelector(".amount input");
+    let valPaisa=paisa.value;
+    if(valPaisa<1||valPaisa===" ")
+    {
+        valPaisa=1;
+        paisa.value="1";
+
+    }
+    const newurl=`${url}/${fromC.value.toLowerCase()}/${toC.value.toLowerCase()}.json`;
+    let response=await fetch(newurl);
+    const data=await response.json();
+    const rate=data[toC.value.toLowerCase()];
+    let finalAmt=rate*valPaisa;
+    info.innerText=`${valPaisa} ${fromC.value}= ${finalAmt} ${toC.value}`
+})
